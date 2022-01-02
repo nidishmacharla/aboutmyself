0a7fc007ba3dce4f84a2ba9d9f571b1352d36120
Nidish macharla <96873321+nidishmacharla@users.noreply.github.com>
2022-01-02 18:10:32 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,17 @@
+/*to find largets number in agiven interger*/
+#include<stdio.h>
+int main()
+{
+	int data,a,lar=0;
+	printf("enter the data:");
+	scanf("%d",&data);
+	while(data>0)
+	{
+		a=data%10;// a gets last value
+		if(a>lar)
+		{
+		lar=a;
+	    }
+		data=data/10;
+	}printf("largest number=%d",lar);
+}
